996ab7700a34616a349d894c651a97edf6c2734e
Ling Tan <lingt.xyz@gmail.com>
2020-05-24 01:23:11 -0400

    update header and separate file

--- README.md
+++ README.md
@@ -3,8 +3,9 @@ Randomly select a percentage of lines of a given file to generate a new file
 
 # Run
 
-`./fileShrinker -i=fn2fn.csv -p=0.6 -s=1`
+`./fileShrinker -i=fn2fn.csv -p=0.6 -h -s`
 
 - `i`: input file is "fn2fn.csv"
 - `p`: shrink percentage is 0.6
-- `s`: lines to skip is 1
+- `h`: keep headers. If true, first line would be appeared in the generated file(s)
+- `s`: keep both files. If true, keep both fn2fn.csv.0.6 and fn2fn.csv.0.4 
--- main.go
+++ main.go
@@ -11,7 +11,8 @@ import (
 func main() {
 	fileName := flag.String("i", "", "fileName of the file to be shrunk")
 	percentage := flag.Float64("p", 0.5, "percentage of the file to keep")
-	skip := flag.Int("s", 0, "lines to skip")
+	keepHeader := flag.Bool("h", false, "keep headers")
+	separate := flag.Bool("s", false, "keep both files")
 	flag.Parse()
 
 	if *fileName == "" {
@@ -24,14 +25,10 @@ func main() {
 		log.Fatalf("Invalid percentage: expecting (0, 1.0), got %v", *percentage)
 	}
 
-	if *skip < 0 {
-		log.Fatalf("Invalid number for lines to skip: expecting > 0, got %v", *skip)
-	}
-
-	shrink(*fileName, *percentage, *skip)
+	shrink(*fileName, *percentage, *keepHeader, *separate)
 }
 
-func shrink(fileName string, percentage float64, skip int) {
+func shrink(fileName string, percentage float64, keepHeader, separate bool) {
 
 	sourceFile, err := os.Open(fileName)
 	if err != nil {
@@ -46,16 +43,37 @@ func shrink(fileName string, percentage float64, skip int) {
 	}
 	defer targetFile.Close()
 
+	var separateFile *os.File
+	if separate {
+		separateFileName := fileName + "." + strconv.FormatFloat(1-percentage, 'f', 2, 64)
+		separateFile, err = os.Create(separateFileName)
+		if err != nil {
+			log.Fatalf("Failed to create target file: %v, got error: %v", separateFile, err)
+		}
+		defer separateFile.Close()
+	}
+
 	scanner := bufio.NewScanner(sourceFile)
-	i := 0
+	var header string
+	var headerScanned bool
 	for scanner.Scan() {
-		if i < skip {
+		if keepHeader && !headerScanned {
+			header = scanner.Text()
+			headerScanned = true
+			_, _ = targetFile.WriteString(header + "\n")
+			_, _ = separateFile.WriteString(header + "\n")
+			continue
+		}
+		if randomize(percentage) {
 			_, _ = targetFile.WriteString(scanner.Text() + "\n")
-			i++
 		} else {
-			if randomize(percentage) {
-				_, _ = targetFile.WriteString(scanner.Text() + "\n")
+			if separate {
+				_, _ = separateFile.WriteString(scanner.Text() + "\n")
 			}
 		}
+	}
+
+	if err := scanner.Err(); err != nil {
+		log.Fatal(err)
 	}
 }
